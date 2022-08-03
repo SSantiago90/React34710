875caf32221e4cf0c74fd61edecd791ea2178b73
ssantiago90 <salkin.santiago@gmail.com>
2022-08-02 21:20:08 -0300

    promises & map

--- src/components/ItemList/ItemListContainer.jsx
+++ src/components/ItemList/ItemListContainer.jsx
@@ -1,38 +1,5 @@
-import React, { useEffect, useState } from "react";
-import itemsData from "../../data/data.js";
-import Card from "../Card/Card.jsx";
-
-function getProductos() {
-  return new Promise((resolve) => {
-    setTimeout(() => resolve(itemsData), 1000);
-  });
-}
+import React from "react";
 
 export default function ItemListContainer() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    getProductos().then((respuesta) => {
-      setData(respuesta);
-    });
-  }, []);
-
-  return (
-    <div>
-      <h1>Item List</h1>
-      {/* ItemList  */}
-      {/* MAPEAR EL STATE */}
-      {data.map((singleMovie) => {
-        return (
-          <Card
-            key={singleMovie.title + singleMovie.category}
-            title={singleMovie.title}
-            category={singleMovie.category}
-            img={singleMovie.img}
-            price={singleMovie.price}
-          />
-        );
-      })}
-    </div>
-  );
+  return <div>ItemListContainer</div>;
 }
